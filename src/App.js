9e8c59bd61caa61a55b988c8dad1857b388c184a
salman898 <s.t.khan701@gmail.com>
2021-03-19 19:22:42 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-    <h1>Hello World Done bootcamp-class-2....</h1>
+    <h1>Hello World Done bootcamp-class-2........</h1>
     </div>
   );
 }
